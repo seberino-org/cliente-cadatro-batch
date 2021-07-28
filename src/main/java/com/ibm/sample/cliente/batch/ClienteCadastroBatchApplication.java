5f0362e79a66e17f641fc8d12caa766a87144ddf
Richard Marques <richard@seberino.com.br>
2021-07-27 22:34:30 -0300

    ajustes de tracing 6

--- src/main/java/com/ibm/sample/cliente/batch/ClienteCadastroBatchApplication.java
+++ src/main/java/com/ibm/sample/cliente/batch/ClienteCadastroBatchApplication.java
@@ -3,8 +3,6 @@ package com.ibm.sample.cliente.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.trace.Tracer;
 
 @SpringBootApplication
 public class ClienteCadastroBatchApplication {
